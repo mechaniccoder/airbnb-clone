435804955e2372430622b1b3d80c53bbdd04500a
mechaniccoder <yuseunghwan94@gmail.com>
2020-04-26 02:02:05 +0900

    pipenv to requirements

--- config/settings.py
+++ config/settings.py
@@ -151,7 +151,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 AUTH_USER_MODEL = "users.User"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
-MEDIA_URL = "/media/"
+MEDIA_URL = "/uploads/"
 
 
 # Email Configuration
