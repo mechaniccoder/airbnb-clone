aa8e53ae61343056b1917c3a0a4d7445f2fe09dc
mechaniccoder <yuseunghwan94@gmail.com>
2020-04-26 01:45:54 +0900

    Fix media URL

--- config/settings.py
+++ config/settings.py
@@ -181,3 +181,4 @@ if DEBUG is False:
 
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
+    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/uploads/"
